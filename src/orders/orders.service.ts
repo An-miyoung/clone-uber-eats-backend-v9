6acfd34bedbbd3345bd7117190d6ccba4a3ddf8b
miyoung <noonchicat@gmail.com>
2023-07-13 15:34:34 +0900

    get orders

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Repository } from 'typeorm';
-import { User } from 'src/users/entities/user.entity';
+import { User, UserRole } from 'src/users/entities/user.entity';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { Restaurant } from 'src/restaurant/entities/restaurant.entity';
 import { OrderItem } from './entities/order-item.entity';
 import { Dish } from 'src/restaurant/entities/dish.entity';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 
 @Injectable()
 export class OrderService {
@@ -96,6 +97,55 @@ export class OrderService {
       return {
         ok: false,
         error: '주문생성에 실패했습니다.',
+      };
+    }
+  }
+
+  async getOrders(
+    user: User,
+    { status }: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    try {
+      let orders: Order[];
+      if (user.role === UserRole.Client) {
+        orders = await this.orders.find({
+          where: {
+            customer: { id: user.id },
+            // entity 에서 default = pending 을 줘서 input 으로 받지 않아도 처리
+            status,
+          },
+        });
+      } else if (user.role === UserRole.Owner) {
+        orders = await this.orders.find({
+          where: {
+            driver: { id: user.id },
+            status,
+          },
+        });
+      } else if (user.role === UserRole.Delivery) {
+        // 한명이 여러개의 restaurant 를 소유할 수 있기때문에 user.id 로 레스토랑을 검색하고
+        // 연결된 orders 를 찾는다.
+        // restaurant = [[order], [order]] 과 같이 중복된 배열의 형태로 돌아온다.
+        const restaurants = await this.restaurants.find({
+          where: { ownerId: user.id },
+          relations: ['orders'],
+        });
+        // owner 가 보유한 레스토랑의 갯수만큼 돌며 order 가 없는 레스토랑의 경우 빈 배열까지 붙여 놓기 때문에 flat 을 이용해 1 depth 내려가서 배열을 다시 만든다.
+        // orders = [[order],[order],[],[],[]] 형태를 [order, order] 형태로
+        orders = restaurants.map((restaurant) => restaurant.orders).flat();
+        if (status) {
+          orders = orders.filter((order) => order.status === status);
+        }
+      }
+
+      return {
+        ok: true,
+        orders,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: '주문목록을 가져오는데 실패했습니다.',
       };
     }
   }
